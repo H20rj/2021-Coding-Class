98c489e1b8b275b7b26a7c6455761bae84be40e6
H20rj <79892001+H20rj@users.noreply.github.com>
2021-03-01 23:00:46 -0600

    Add files via upload

---
+++ NumberGuessing/NumberGuessing/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace NumberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number inbetween 0 and 100: ");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("Too high! Guess lower...");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("Too low! Guess higher...");
+                }
+                else if (i == winNum)
+                {
+                    Console.WriteLine("YOU WIN!");
+                    win = true;
+                }
+
+                Console.WriteLine();
+
+
+            } while (win == false);
+
+            Console.WriteLine("Thank you for playing the game!");
+            Console.Write("Press any key to finish.");
+            Console.ReadKey(true);
+        }
+    }
+}
